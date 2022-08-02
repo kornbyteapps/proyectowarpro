c691abb8a9415e28ed447b422d7bcccfd850ac48
kornbyteapps <69880819+kornbyteapps@users.noreply.github.com>
2022-08-02 11:32:20 -0400

    Delete ti_manager directory

--- ti_manager/api/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from api import models
-
-admin.site.register(models.UserProfile)
-admin.site.register(models.users)
-# Register your models here.
--- ti_manager/api/serializers.py
+++
@@ -1,7 +0,0 @@
-from unittest.util import _MAX_LENGTH
-from rest_framework import serializers
-
-class HelloSerializer(serializers.Serializer):
-    # serializador para metodo post
-    name = serializers.CharField(maxlength=200)
-    email = serializers.EmailField()
